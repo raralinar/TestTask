0dac2f9acf6ae3dbf10dad5de7c0efb6039df5ba
raralinar <raralinars@gmail.com>
2024-01-16 09:00:14 +0300

    Output format changed

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException, ParseException, java.text.ParseException {
         Flights flightTimes = new Flights(ReadJsonFile.readFile("tickets.json"));
-        System.out.println("Минимальное время полета между городами Владивосток и Тель-Авив для каждого авиаперевозчика: " +
+        System.out.println("Минимальное время полета между городами Владивосток и Тель-Авив для каждого авиаперевозчика (в минутах): " +
                 flightTimes.getMin() + "\n" +
                 "Разницу между средней ценой  и медианой для полета между городами  Владивосток и Тель-Авив: " +
                 flightTimes.getPriceDifference());
